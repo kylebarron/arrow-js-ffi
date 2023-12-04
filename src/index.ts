57d8978fc0a967842bdba5b2c84e97e450710651
Kyle Barron <kylebarron2@gmail.com>
2023-12-03 22:39:30 -0500

    Set copy to true by default (#71)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { parseVector } from "./vector";
+export { parseVector, parseData } from "./vector";
 export { parseField } from "./field";
 export { parseRecordBatch } from "./record-batch";
